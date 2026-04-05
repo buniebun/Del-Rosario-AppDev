7c1842f5a3db2fd9f24b4f0443bc9f500ae3df96
buniebun <delrosariodivinerose@gmail.com>
2026-04-06 00:12:30 +0800

    Add project files.

---
+++ Appdev2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Appdev2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
